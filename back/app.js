ba0be17b6012eb79d3099feb54c62aaf14e75d15
Catherine <catblecon@gmail.com>
2020-06-20 12:07:30 +0100

    quest030

---
+++ back/app.js
@@ -0,0 +1,31 @@
+// declare all the necessary libraries
+const  http  =  require('http');
+const  path  =  require('path');
+const  express  =  require('express');
+const  bodyParser  =  require('body-parser');
+const  morgan  =  require('morgan');
+const authRouter = require('./routes/auth/auth');
+
+const  app  =  express();
+
+// set up the application
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended:  false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname  +  '/public'));
+
+// j'implémente la partie API
+app.use('/auth', authRouter);
+
+/// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
+app.use(function(req, res, next) {
+    var  err  =  new  Error('Not Found');
+    err.status  =  404;
+    next(err);
+});
+
+
+// launch the node server
+let  server  =  app.listen( process.env.PORT  ||  3000, function(){
+    console.log('Listening on port '  +  server.address().port);
+});
