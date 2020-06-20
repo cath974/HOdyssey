3036e2be0e07317250b693d83237f06636368aa6
Catherine <catblecon@gmail.com>
2020-06-20 20:36:50 +0100

    quest060

--- front/reacthomer/src/App.js
+++ front/reacthomer/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import SignUp from './components/SignUp/SignUp';
-
-
+// import { MuiThemeProvider} from '@material-ui/core/styles';
+import { Grid , Paper } from '@material-ui/core';
 function App() {
   return (
+
     <div className="App">
-      <SignUp />
+              {/* <MuiThemeProvider  > */}
+                <Grid  container alignItems='center' style={{ height:  '100%' }}>
+                  <Grid  item  xs={12}>
+                        <Paper elevation={4} style={{ margin:  32 }}>
+                            <Grid  container alignItems='center' justify='center'>
+                                <Grid  item  xs={12} sm={6} style={{ 'textAlign':  'center' }}>
+                                    <img  src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png" alt='homer' />
+                                </Grid>  
+                                
+                                <Grid  item  xs={12} sm={6} >
+                                    <SignUp  />
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                </Grid>
+        {/* </MuiThemeProvider> */}
     </div>
   );
 }
--- front/reacthomer/src/components/SignUp/SignUp.js
+++ front/reacthomer/src/components/SignUp/SignUp.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
+import { TextField, Button, SnackbarContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 300,
+      display: "flex",
+      flexDirection: "column"
+    },
+    '& .MuiButtonBase-root': {
+        marginLeft: theme.spacing(30),
+        marginBottom:  theme.spacing(30),
+        width: 30,
+      },
+
+  },
+}));
 
 const SignUp = () => {
+
+    const classes = useStyles();
    
     const [ signUP, setSignUp ] = useState({
     email: '',
     password:'',
     passwordbis: '',
     name:'',
-    lastname: ''
+    lastname: '',
+    flash:''
     }); 
 
  const  updateField = (event) => {
@@ -18,25 +40,42 @@ const SignUp = () => {
 
   const handleSubmit = (event) => {
       event.preventDefault();
-      console.log (signUP)
+                fetch("/auth/signup",
+            {
+                method:  'POST',
+                headers:  new  Headers({
+                    'Content-Type':  'application/json'
+                }),
+                body:  JSON.stringify(signUP),
+            })
+            .then(res  =>  res.json())
+            .then(
+                res  =>  setSignUp({...signUP, flash: res.flash }),
+                err  =>  setSignUp({...signUP, flash: err.flash })
+            )
+            console.log (signUP)
   }
 
 
       return (
         <div>
-            <h1>{JSON.stringify(signUP,1,1)}</h1>
-                <form onSubmit={handleSubmit}>
-                <input type="email" name="email" value={signUP.email} onChange={updateField}/>
-                <input type="password" name="password" value={signUP.password} onChange={updateField}/>
-                <input type="password" name="passwordbis" value={signUP.passwordbis} onChange={updateField}/>
-                <input type="text" name="name" value={signUP.name} onChange={updateField}/>
-                <input type="text" name="lastname" value={signUP.lastname} onChange={updateField}/>
-                <input type="submit" name="submit"/>
+            {/* <h1>{JSON.stringify(signUP,1,1)}</h1> */}
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField id="standard-basic"  label="Email" type="email" name="email" required value={signUP.email} onChange={updateField}/>
+                <TextField id="standard-basic"  label="Password" type="password" name="password" required value={signUP.password} onChange={updateField}/>
+                <TextField id="standard-basic"  label="Password Copy" type="password" name="passwordbis" required value={signUP.passwordbis} onChange={updateField}/>
+                <TextField id="standard-basic"  label="Name" type="text" name="name" value={signUP.name} required onChange={updateField}/>
+                <TextField id="standard-basic"  label="LastName" type="text" name="lastname" required value={signUP.lastname} onChange={updateField}/>
+                <Button variant="contained" color="primary" type="submit" name="submit">Submit</Button>
                 </form>
-                       
+               <div> 
+                   {signUP.flash &&  <SnackbarContent message={signUP.flash} />}
+               </div>
+         
         </div>          
                    
     )
 }
 
 export default SignUp;
+
