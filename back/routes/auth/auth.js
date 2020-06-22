ec065c3952003989beed7e8a964e3b57a6c85913
Catherine <catblecon@gmail.com>
2020-06-22 11:22:17 +0100

    quest070

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -18,5 +18,21 @@ router.post('/signup', function(req, res, next) {
                   .json({ flash:  `User has been signed up !` });
               });
             });
+
+  router.post('/signin', function(req, res, next) {
+    // let userData = req.body;
+    // const {passwordbis,flash, ...userData} = req.body;
+    // connection.query('INSERT INTO users SET ?', userData, (err, results) => {
+    //     if (err) {
+    //         return res.status(500).json({
+    //           flash: err.message,
+    //           sql: err.sql,
+    //         });
+    //       }
+    //         return res
+    //           .status(201)
+    //           .json({ flash:  `User has been signed up !` });
+    //       });
+        });           
      
 module.exports = router;
