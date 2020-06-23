da95f4e27cb5a30d5b3f5495b710f0a998a8e06e
Catherine <catblecon@gmail.com>
2020-06-23 16:08:51 +0100

    quest080

--- back/app.js
+++ back/app.js
@@ -6,8 +6,45 @@ const  bodyParser  =  require('body-parser');
 const  morgan  =  require('morgan');
 const authRouter = require('./routes/auth/auth');
 const connection = require('./helpers/db');
-
+const LocalStrategy = require('passport-local').Strategy
+const passport = require('passport')
+const passportJWT = require("passport-jwt");
+const bcrypt = require('bcrypt')
 const  app  =  express();
+const ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+
+
+passport.use('local', new LocalStrategy({
+        usernameField: 'email',
+         passwordField: 'password',
+         session: false
+  } , function (email, password, done){
+        
+        connection.query('select * from users where email = ?', [email], function(err, result){
+            console.log(err); console.log(result);
+          if (err) return done(err);
+          if(!result.length){ 
+              return done(null, false, {message: 'Invalid email'})
+             };
+          if  (bcrypt.compareSync(password, result[0].password)){
+            return done(null, result[0])
+    
+            } else {
+                return done(null, false, {message: 'Password incorrect'})
+            }
+        })   
+        })) 
+
+passport.use(new JWTStrategy({ 
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),  
+    secretOrKey   : 'coucou',  
+},  
+function (jwtPayload, done) {  
+    return done(null, jwtPayload);
+  }  
+));
+
 
 // set up the application
 app.use(morgan('dev'));
@@ -17,7 +54,10 @@ app.use(express.static(__dirname  +  '/public'));
 
 // j'implémente la partie API
 app.use('/auth', authRouter);
-
+app.get('/profile', passport.authenticate('jwt', { session:  false }),function (req, res) {
+    res.send(req.user);
+    })
+    
 /// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
 app.use(function(req, res, next) {
     var  err  =  new  Error('Not Found');
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../../helpers/db');
+const bcrypt=require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
 
 router.post('/signup', function(req, res, next) {
         
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  //const isSame = bcrypt.compareSync('somePassword', hash)
+
         // let userData = req.body;
-        const {passwordbis,flash, ...userData} = req.body;
-        connection.query('INSERT INTO users SET ?', userData, (err, results) => {
+        //const {passwordbis,flash, ...userData} = req.body;
+        const { email, name, lastname } = req.body;
+        const userValue= [email, hash, name, lastname ]
+  
+        // connection.query('INSERT INTO users SET ?', userData, (err, results) => {
+        connection.query('INSERT INTO users (email, password, name, lastname) VALUES (?, ?, ?, ?)', userValue, (err, results) => {
             if (err) {
                 return res.status(500).json({
                   flash: err.message,
@@ -19,20 +30,17 @@ router.post('/signup', function(req, res, next) {
               });
             });
 
-  router.post('/signin', function(req, res, next) {
-    // let userData = req.body;
-    // const {passwordbis,flash, ...userData} = req.body;
-    // connection.query('INSERT INTO users SET ?', userData, (err, results) => {
-    //     if (err) {
-    //         return res.status(500).json({
-    //           flash: err.message,
-    //           sql: err.sql,
-    //         });
-    //       }
-    //         return res
-    //           .status(201)
-    //           .json({ flash:  `User has been signed up !` });
-    //       });
-        });           
+ 
+    router.post('/signin', function(req, res, next) {
+          passport.authenticate('local',(err, user, info) => { 
+            if(err) return res.status(500).send(err) 
+            if (!user) return res.status(400).json({message: info.message});  
+            // return res.json({user}); 
+            const token = jwt.sign(JSON.stringify(user), 'coucou');  
+            return res.json({user, token}); 
+        })(req, res);
+      })
+   
+            
      
 module.exports = router;
